3e19087c4a0b96fe8580c9a4f62f843322c72713
Eugene <nikelodiym1210@gmail.com>
2024-06-14 09:13:22 +0300

    02/06

---
+++ lesson-02/06.js
@@ -0,0 +1,16 @@
+console.log(1)
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+const copied = structuredClone(passportWithAddress)
+copied.address.city = 'Bobryisk';
+
+
+console.log(passportWithAddress.address.city);
+console.log(copied.address.city);
